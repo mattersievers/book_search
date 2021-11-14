14c7b60c429a15d219019aa4b9983c5d0bdd1f2c
Matthew Sievers <mattersievers@gmail.com>
2021-11-14 05:40:18 -0600

    fixed auth function and can addUser and query User successfully

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,13 +4,29 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) =>{
+            if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-_v -password')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
         users: async () => {
             return User.find()
             .select('-__v -password')
         }
+        
     },
     Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return { token, user };
+        }
     }
 
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,15 +4,33 @@ const { gql } = require('apollo-server-express');
 // create typeDefs
 const typeDefs = gql`
     type User {
-        _id:ID
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: String
+        description: String
+        title: String
+        image: String
+        link: String
     }
 
     type Query {
-     users:[User]
+        me: User
+        users: [User]
+        books: [Book]
     }
 
     type Mutation {
         login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(userId: ID!, bookId: ID!): User
+        removeBook(userId: ID!, bookId: ID!): User
     }
 
     type Auth {
