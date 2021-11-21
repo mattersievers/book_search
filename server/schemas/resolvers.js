ad618d786929d9ee5283aed7d19bc74b3c313852
Matthew Sievers <mattersievers@gmail.com>
2021-11-20 18:22:23 -0600

    removeBook functions, clsoing branch to work on front end

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -65,11 +65,10 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },    
         removeBook: async(parent, args, context) => {
-            console.log(args.bookId);
             if(context.user) {
                 const updatedUser = await User.findByIdAndUpdate(
                     { _id: context.user._id },
-                    { savedBooks: savedBooks.filter(book => book.bookId !== args.bookId ) },
+                    { $pull: {savedBooks: { bookId: args.bookId } } },
                     { new:true }
                 );
                 return updatedUser;
